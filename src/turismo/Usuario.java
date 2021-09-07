bd309e64a10ac0ed6e899eed5cc5ce985387b1d4
dantemogni <dantejoaquin.mogni@gmail.com>
2021-09-07 17:26:08 -0300

    Creacion Constuctor

--- src/turismo/Usuario.java
+++ src/turismo/Usuario.java
@@ -9,7 +9,17 @@ public class Usuario {
 	Itinerario itinerarioPersonal;
 	TipoAtraccion preferenciaDelUsuario;
 	
-	
+	public Usuario(int dni, String nombreDeUsuario, double presupuesto, 
+			double tiempoDisponible, TipoAtraccion preferenciaDelUsuario) {
+		
+		this.dni = dni;
+		this.nombreDeUsuario = nombreDeUsuario;
+		this.presupuesto = presupuesto;
+		this.tiempoDisponible = tiempoDisponible;
+		this.preferenciaDelUsuario = preferenciaDelUsuario;
+		
+		//TODO: intinerarioPersonal ?
+	}
 
 
 }
