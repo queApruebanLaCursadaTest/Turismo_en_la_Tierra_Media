11930cb427031e2098c11970d6fa661f1d24ac3c
Dante Mogni <dantejoaquin.mogni@gmail.com>
2021-09-28 20:21:48 -0300

    Modificado y arreglado para la nueva implementacion de la clase Sugerencia

--- src/turismo/ComparadorDeSugerencias.java
+++ src/turismo/ComparadorDeSugerencias.java
@@ -2,39 +2,39 @@ package turismo;
 
 import java.util.Comparator;
 
-public class Comparador implements Comparator<Sugerencia> {
-	TipoAtraccion atraccionFavorita;
+public class ComparadorDeSugerencias implements Comparator<Sugerencia> {
+	private TipoAtraccion atraccionFavorita;
 
-	public Comparador(TipoAtraccion atraccionFavorita) {
+	public ComparadorDeSugerencias(TipoAtraccion atraccionFavorita) {
 		this.atraccionFavorita = atraccionFavorita;
 	}
 
 	@Override
 	public int compare(Sugerencia o1, Sugerencia o2) {
+		// Prioridad preferencia
+		if (this.atraccionFavorita == o2.getTipo() && this.atraccionFavorita != o1.getTipo()) {
+			return 1;
+		}
+		if (this.atraccionFavorita == o1.getTipo() && this.atraccionFavorita != o2.getTipo()) {
+			return -1;
+		}
+		
 		// Prioridad promocion
 		if (o2.esPromocion() && !o1.esPromocion()) {
 			return 1;
 		}
-		if (!o2.esPromocion() && o1.esPromocion()) {
-			return -1;
-		}
-
-		// Prioridad atraccion favorita
-		if (o2.getAtraccionSugerida() == atraccionFavorita && o1.getAtraccionSugerida() != atraccionFavorita) {
+		if (o1.esPromocion() && !o2.esPromocion()) {
 			return 1;
-		}
-		if (o2.getAtraccionSugerida() != atraccionFavorita && o1.getAtraccionSugerida() == atraccionFavorita) {
-			return -1;
 		}
 
 		// Prioridad por dinero
-		double diferenciaCosto = o2.getPrecioDeLaOferta() - o1.getPrecioDeLaOferta();
+		double diferenciaCosto = o2.getCosto() - o1.getCosto();
 		if (diferenciaCosto != 0) {
 			return diferenciaCosto > 0 ? 1 : (diferenciaCosto < 0 ? -1 : 0);
 		}
 
 		// Prioridad por tiempo
-		double diferenciaTiempo = o2.getTiempoDeLaOferta() - o1.getTiempoDeLaOferta();
+		double diferenciaTiempo = o2.getTiempoRequerido() - o1.getTiempoRequerido();
 		return diferenciaTiempo > 0 ? 1 : (diferenciaTiempo < 0 ? -1 : 0);
 	}
 
