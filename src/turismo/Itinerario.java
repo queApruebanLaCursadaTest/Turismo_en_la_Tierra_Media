5b5c73831f6ed0efa0283185a9ffccd3b0d1be5a
Dante Mogni <dantejoaquin.mogni@gmail.com>
2021-09-28 21:24:28 -0300

    Se elimino el constructor

--- src/turismo/Itinerario.java
+++ src/turismo/Itinerario.java
@@ -7,10 +7,6 @@ public class Itinerario {
 	
 	List <Sugerencia> listadoSugerenciasAceptadas = new LinkedList<Sugerencia>();
 
-	public Itinerario(List<Sugerencia> listadoSugerenciasAceptadas) {
-		this.listadoSugerenciasAceptadas = listadoSugerenciasAceptadas;
-	}
-	
 	public boolean aceptarSugerencia(Sugerencia nueva) {
 		return listadoSugerenciasAceptadas.add(nueva); // devuelve true si se agregó
 	}
